c289e5fa0139d25c00ee4ebb026c7d09289e2692
QQ652276536 <652276536@qq.com>
2020-08-10 12:34:43 +0800

    图片的人脸属性检测可以了

--- app/src/main/java/com/zistone/factorytest0718/FaceAttributeMenuActivity.java
+++ app/src/main/java/com/zistone/factorytest0718/FaceAttributeMenuActivity.java
@@ -2,6 +2,8 @@ package com.zistone.factorytest0718;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,9 +16,15 @@ import androidx.core.app.ActivityCompat;
 import com.arcsoft.face.ActiveFileInfo;
 import com.arcsoft.face.ErrorInfo;
 import com.arcsoft.face.FaceEngine;
+import com.arcsoft.face.VersionInfo;
 import com.arcsoft.face.enums.RuntimeABI;
 import com.zistone.factorytest0718.face.ChooseDetectDegreeDialog;
 import com.zistone.factorytest0718.face.Constants;
+import com.zistone.factorytest0718.face.SingleImageActivity;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
@@ -43,17 +51,11 @@ public class FaceAttributeMenuActivity extends BaseActivity implements View.OnCl
     private TextView _txt;
     private Button _btnSetting, _btnActive, _btnFaceAttributeForImage, _btnFaceAttributeForVideo;
     private ChooseDetectDegreeDialog _chooseDetectDegreeDialog;
-    private boolean _libraryExists = true;
 
     /**
      * 激活
      */
     public void ActiveEngine() {
-        //所需要的SO库不存在
-        if (!_libraryExists) {
-            Toast.makeText(this, "未找到库文件，请检查是否有将.so文件放至工程的 app\\src\\main\\jniLibs 目录下", Toast.LENGTH_SHORT).show();
-            return;
-        }
         //未授予权限
         if (!CheckPermissions(NEEDED_PERMISSIONS)) {
             ActivityCompat.requestPermissions(this, NEEDED_PERMISSIONS, ACTION_REQUEST_PERMISSIONS);
@@ -78,11 +80,11 @@ public class FaceAttributeMenuActivity extends BaseActivity implements View.OnCl
             @Override
             public void onNext(Integer activeCode) {
                 if (activeCode == ErrorInfo.MOK) {
-                    Toast.makeText(FaceAttributeMenuActivity.this, "激活成功", Toast.LENGTH_SHORT).show();
+                    _txt.setText("激活成功");
                 } else if (activeCode == ErrorInfo.MERR_ASF_ALREADY_ACTIVATED) {
-                    Toast.makeText(FaceAttributeMenuActivity.this, "引擎已激活", Toast.LENGTH_SHORT).show();
+                    _txt.setText("引擎已激活");
                 } else {
-                    Toast.makeText(FaceAttributeMenuActivity.this, "激活失败，错误码为：" + activeCode, Toast.LENGTH_SHORT).show();
+                    _txt.setText("激活失败，错误码为：" + activeCode);
                 }
                 ActiveFileInfo activeFileInfo = new ActiveFileInfo();
                 int res = FaceEngine.getActiveFileInfo(FaceAttributeMenuActivity.this, activeFileInfo);
@@ -93,7 +95,7 @@ public class FaceAttributeMenuActivity extends BaseActivity implements View.OnCl
 
             @Override
             public void onError(Throwable e) {
-                Toast.makeText(FaceAttributeMenuActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                _txt.setText(e.getMessage());
             }
 
             @Override
@@ -121,6 +123,7 @@ public class FaceAttributeMenuActivity extends BaseActivity implements View.OnCl
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            //设置检测角度
             case R.id.btn_setting_face_attribute_menu:
                 if (_chooseDetectDegreeDialog == null) {
                     _chooseDetectDegreeDialog = new ChooseDetectDegreeDialog();
@@ -130,14 +133,41 @@ public class FaceAttributeMenuActivity extends BaseActivity implements View.OnCl
                 }
                 _chooseDetectDegreeDialog.show(getSupportFragmentManager(), ChooseDetectDegreeDialog.class.getSimpleName());
                 break;
+            //激活引擎
             case R.id.btn_active_face_attribute_menu:
                 ActiveEngine();
                 break;
+            //显示图片中所有人脸的信息，并且逐一比对相似度
             case R.id.btn_face_attribute_image_menu:
+                startActivity(new Intent(this, SingleImageActivity.class));
                 break;
+            //显示视频中所有人脸信息
             case R.id.btn_face_attribute_video_menu:
                 break;
         }
+    }
+
+    /**
+     * 检查能否找到动态链接库
+     *
+     * @param
+     * @return
+     */
+    private boolean CheckSoFile(String[] libraries) {
+        File dir = new File(getApplicationInfo().nativeLibraryDir);
+        File[] files = dir.listFiles();
+        if (files == null || files.length == 0) {
+            return false;
+        }
+        List<String> libraryNameList = new ArrayList<>();
+        for (File file : files) {
+            libraryNameList.add(file.getName());
+        }
+        boolean exists = true;
+        for (String library : libraries) {
+            exists &= libraryNameList.contains(library);
+        }
+        return exists;
     }
 
     @Override
@@ -145,7 +175,9 @@ public class FaceAttributeMenuActivity extends BaseActivity implements View.OnCl
         super.onCreate(savedInstanceState);
         //        setContentView(R.layout.activity_bankcard);
         SetBaseContentView(R.layout.activity_face_attribute_menu);
-        _txt = findViewById(R.id.txt_bankcard);
+        ApplicationInfo applicationInfo = getApplicationInfo();
+        Log.i(TAG, "onCreate: " + applicationInfo.nativeLibraryDir);
+        _txt = findViewById(R.id.txt_face_attribute_menu);
         _btnSetting = findViewById(R.id.btn_setting_face_attribute_menu);
         _btnSetting.setOnClickListener(this::onClick);
         _btnActive = findViewById(R.id.btn_active_face_attribute_menu);
@@ -166,6 +198,14 @@ public class FaceAttributeMenuActivity extends BaseActivity implements View.OnCl
             setResult(RESULT_OK, intent);
             finish();
         });
+        if (!CheckSoFile(LIBRARIES)) {
+            _txt.setText("未找到库文件，请检查是否有将.so文件放至工程的app\\src\\main\\jniLibs目录下");
+            _txt.setTextColor(Color.RED);
+            _btnActive.setEnabled(false);
+            _btnSetting.setEnabled(false);
+            _btnFaceAttributeForImage.setEnabled(false);
+            _btnFaceAttributeForVideo.setEnabled(false);
+        }
     }
 
 }
