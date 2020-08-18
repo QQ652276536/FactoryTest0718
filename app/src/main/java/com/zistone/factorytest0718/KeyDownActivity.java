7d549e02c383e965e19f261eefe0bcdbf1a26462
QQ652276536 <652276536@qq.com>
2020-08-18 17:37:01 +0800

    1、按键测试加入WD220B对应的控件 2、一键拨号和电源键目前监听不到，需要特殊处理

--- app/src/main/java/com/zistone/factorytest0718/KeyDownActivity.java
+++ app/src/main/java/com/zistone/factorytest0718/KeyDownActivity.java
@@ -19,6 +19,9 @@ public class KeyDownActivity extends BaseActivity {
 
     private static final String TAG = "KeyDownActivity";
 
+    //型号WD220B所对应的控件
+    private TextView _txt1Wd220b, _txt2Wd220b, _txt3Wd220b, _txt4Wd220b, _txt5Wd220b, _txt6Wd220b;
+    private LinearLayout _llWd220b;
     private TextView _txt;
     private ImageView _iv;
 
@@ -39,6 +42,16 @@ public class KeyDownActivity extends BaseActivity {
         _iv.setVisibility(View.VISIBLE);
         if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
             this.finish();
+        } else if (keyCode == KeyEvent.KEYCODE_VOLUME_UP && event.getAction() == KeyEvent.ACTION_DOWN) {
+            _txt1Wd220b.setBackgroundColor(SPRING_GREEN);
+        } else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN && event.getAction() == KeyEvent.ACTION_DOWN) {
+            _txt2Wd220b.setBackgroundColor(SPRING_GREEN);
+        } else if (keyCode == KeyEvent.KEYCODE_F2 && event.getAction() == KeyEvent.ACTION_DOWN) {
+            _txt3Wd220b.setBackgroundColor(SPRING_GREEN);
+        } else if (keyCode == KeyEvent.KEYCODE_UNKNOWN && event.getAction() == KeyEvent.ACTION_DOWN) {
+            _txt5Wd220b.setBackgroundColor(SPRING_GREEN);
+        } else if (keyCode == KeyEvent.KEYCODE_POWER && event.getAction() == KeyEvent.ACTION_DOWN) {
+            _txt6Wd220b.setBackgroundColor(SPRING_GREEN);
         }
         return false;
     }
@@ -49,6 +62,14 @@ public class KeyDownActivity extends BaseActivity {
         //        setContentView(R.layout.activity_keydown);
         SetBaseContentView(R.layout.activity_keydown);
         _txt = findViewById(R.id.txt_keydown);
+        //型号WD220B对应的控件
+        _llWd220b = findViewById(R.id.ll_wd220b_keydown);
+        _txt1Wd220b = findViewById(R.id.txt1_wd220b_keydown);
+        _txt2Wd220b = findViewById(R.id.txt2_wd220b_keydown);
+        _txt3Wd220b = findViewById(R.id.txt3_wd220b_keydown);
+        _txt4Wd220b = findViewById(R.id.txt4_wd220b_keydown);
+        _txt5Wd220b = findViewById(R.id.txt5_wd220b_keydown);
+        _txt6Wd220b = findViewById(R.id.txt6_wd220b_keydown);
         _iv = findViewById(R.id.iv_keydown);
     }
 }
