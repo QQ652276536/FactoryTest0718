b2f72fb2a1cefa39fee6919b16dafd126d3f0c56
QQ652276536 <652276536@qq.com>
2020-08-19 17:08:06 +0800

    GPS改为实时获取并且经纬度小数位数做了限制

--- app/src/main/java/com/zistone/factorytest0718/GpsActivity.java
+++ app/src/main/java/com/zistone/factorytest0718/GpsActivity.java
@@ -32,6 +32,7 @@ public class GpsActivity extends BaseActivity implements LocationListener {
     private GeocodeTask _geocodeTask;
     private LocationManager _locationManager;
     private MyLocationListener _myLocationListener;
+    private boolean _isThreadRun = false;
 
     public interface MyLocationListener {
         void OnLocationChanged(Location location);
@@ -91,49 +92,20 @@ public class GpsActivity extends BaseActivity implements LocationListener {
          */
         @Override
         protected void onCancelled() {
+            _isThreadRun = true;
         }
     }
 
     /**
      * 开始定位
      *
-     * @param isOpenGpsSetting 是否打开GPS设置界面
+     * @param providerList
      * @throws Exception
      */
-    private void Start(boolean isOpenGpsSetting) {
-        _locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        //检查权限
+    private void Start(List<String> providerList) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Log.e(TAG, "GPS打开失败，检查是否授予权限");
             return;
         }
-        //位置更新的最短时间为10秒，最短距离为1米
-        _locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10 * 1000, 1, this);
-        boolean isOpenGps = _locationManager.isProviderEnabled(LOCATETYPE);
-        //位置没打开，是否打开位置设置界面
-        if (!isOpenGps && isOpenGpsSetting && Build.VERSION.SDK_INT > 15) {
-            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            startActivityForResult(intent, 101);
-            return;
-        }
-        _myLocationListener.OnUpdateProviderStatus(1);
-        //通过设备支持的定位方式来获得位置信息
-        List<String> providerList = _locationManager.getProviders(true);
-        //GPS定位，通过卫星获取定位信息
-        String providerStr = "";
-        if (providerList.contains(LocationManager.GPS_PROVIDER)) {
-            providerStr += LocationManager.GPS_PROVIDER.toUpperCase() + "、";
-        }
-        //网络定位，通过基站和wifi获取定位信息
-        if (providerList.contains(LocationManager.NETWORK_PROVIDER)) {
-            providerStr += LocationManager.NETWORK_PROVIDER.toUpperCase() + "、";
-        }
-        //被动定位，第三方应用使用了定位系统会保存下来，通过此方式可以获取最近一次位置信息
-        if (providerList.contains(LocationManager.PASSIVE_PROVIDER)) {
-            providerStr += LocationManager.PASSIVE_PROVIDER.toUpperCase();
-        }
-        Log.i(TAG, "该设备支持的位置提供器：" + providerStr);
-        _myLocationListener.OnUpdateProviders(providerStr);
         Location location = null;
         for (String temp : providerList) {
             Location tempLocation = _locationManager.getLastKnownLocation(temp);
@@ -210,6 +182,7 @@ public class GpsActivity extends BaseActivity implements LocationListener {
     protected void onDestroy() {
         super.onDestroy();
         _locationManager.removeUpdates(this);
+        _isThreadRun = true;
     }
 
     @Override
@@ -238,34 +211,95 @@ public class GpsActivity extends BaseActivity implements LocationListener {
         _myLocationListener = new MyLocationListener() {
             @Override
             public void OnLocationChanged(Location location) {
-                _txtLat.setText(location.getLatitude() + "");
-                _txtLot.setText(location.getLongitude() + "");
-                _geocodeTask = new GeocodeTask();
-                _geocodeTask.execute(new Double[]{location.getLatitude(), location.getLongitude()});
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        _txtLat.setText(String.format("%.6f", location.getLatitude()));
+                        _txtLot.setText(String.format("%.6f", location.getLongitude()));
+                        _geocodeTask = new GeocodeTask();
+                        _geocodeTask.execute(new Double[]{location.getLatitude(), location.getLongitude()});
+                    }
+                });
             }
 
             @Override
             public void OnUpdateProviderStatus(int status) {
-                switch (status) {
-                    case 0:
-                        _txtState.setText("定位服务关闭");
-                        _txtState.setTextColor(Color.RED);
-                        _btnPass.setEnabled(false);
-                        break;
-                    case 1:
-                        _txtState.setText("定位服务开启");
-                        _txtState.setTextColor(SPRING_GREEN);
-                        _btnPass.setEnabled(true);
-                        break;
-                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        switch (status) {
+                            case 0:
+                                _txtState.setText("定位服务关闭");
+                                _txtState.setTextColor(Color.RED);
+                                _btnPass.setEnabled(false);
+                                break;
+                            case 1:
+                                _txtState.setText("定位服务开启");
+                                _txtState.setTextColor(SPRING_GREEN);
+                                _btnPass.setEnabled(true);
+                                break;
+                        }
+                    }
+                });
             }
 
             @Override
             public void OnUpdateProviders(String providers) {
-                _txtProvider.setText(providers);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        _txtProvider.setText(providers);
+                    }
+                });
             }
         };
-        Start(true);
+        _locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        //检查权限
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.e(TAG, "GPS打开失败，检查是否授予权限");
+            return;
+        }
+        //位置更新的最短时间为10秒，最短距离为1米
+        _locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10 * 1000, 1, this);
+        boolean isOpenGps = _locationManager.isProviderEnabled(LOCATETYPE);
+        //位置没打开，是否打开位置设置界面
+        if (!isOpenGps && Build.VERSION.SDK_INT > 15) {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivityForResult(intent, 101);
+            return;
+        }
+        _myLocationListener.OnUpdateProviderStatus(1);
+        //通过设备支持的定位方式来获得位置信息
+        List<String> providerList = _locationManager.getProviders(true);
+        //GPS定位，通过卫星获取定位信息
+        String providerStr = "";
+        if (providerList.contains(LocationManager.GPS_PROVIDER)) {
+            providerStr += LocationManager.GPS_PROVIDER.toUpperCase() + "、";
+        }
+        //网络定位，通过基站和wifi获取定位信息
+        if (providerList.contains(LocationManager.NETWORK_PROVIDER)) {
+            providerStr += LocationManager.NETWORK_PROVIDER.toUpperCase() + "、";
+        }
+        //被动定位，第三方应用使用了定位系统会保存下来，通过此方式可以获取最近一次位置信息
+        if (providerList.contains(LocationManager.PASSIVE_PROVIDER)) {
+            providerStr += LocationManager.PASSIVE_PROVIDER.toUpperCase();
+        }
+        Log.i(TAG, "该设备支持的位置提供器：" + providerStr);
+        _myLocationListener.OnUpdateProviders(providerStr);
+        //实时定位
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!_isThreadRun) {
+                    try {
+                        Start(providerList);
+                        Thread.sleep(5 * 1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 
 }
