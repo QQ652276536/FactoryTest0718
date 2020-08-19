41ac6a3616c56606982751430db7acd80fe9ea9b
QQ652276536 <652276536@qq.com>
2020-08-19 14:25:00 +0800

    所有按键按下才允许测试通过

--- app/src/main/java/com/zistone/factorytest0718/KeyDownActivity.java
+++ app/src/main/java/com/zistone/factorytest0718/KeyDownActivity.java
@@ -1,5 +1,7 @@
 package com.zistone.factorytest0718;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,26 +11,27 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.zistone.factorytest0718.util.HomeKeyObserver;
-import com.zistone.factorytest0718.util.PowerKeyObserver;
+import com.zistone.factorytest0718.util.MyProgressDialogUtil;
 
 public class KeyDownActivity extends BaseActivity {
 
     private static final String TAG = "KeyDownActivity";
 
     //型号WD220B所对应的控件
-    private TextView _txt1Wd220b, _txt2Wd220b, _txt3Wd220b, _txt4Wd220b, _txt5Wd220b, _txt6Wd220b;
+    private TextView _txt1Wd220b, _txt2Wd220b, _txt3Wd220b, _txt4Wd220b, _txt5Wd220b;
     private LinearLayout _llWd220b;
     private TextView _txt;
     private ImageView _iv;
-    private HomeKeyObserver _homeKeyObserver;
-    private PowerKeyObserver _powerKeyObserver;
+    private boolean[] _keyPasss = new boolean[]{false, false, false, false, false};
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        _homeKeyObserver.stopListen();
-        _powerKeyObserver.stopListen();
     }
 
     @Override
@@ -50,15 +53,34 @@ public class KeyDownActivity extends BaseActivity {
             this.finish();
         } else if (keyCode == KeyEvent.KEYCODE_VOLUME_UP && event.getAction() == KeyEvent.ACTION_DOWN) {
             _txt1Wd220b.setBackgroundColor(SPRING_GREEN);
+            _keyPasss[0] = true;
         } else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN && event.getAction() == KeyEvent.ACTION_DOWN) {
             _txt2Wd220b.setBackgroundColor(SPRING_GREEN);
+            _keyPasss[1] = true;
         } else if (keyCode == KeyEvent.KEYCODE_F2 && event.getAction() == KeyEvent.ACTION_DOWN) {
             _txt3Wd220b.setBackgroundColor(SPRING_GREEN);
+            _keyPasss[2] = true;
         } else if (keyCode == KeyEvent.KEYCODE_UNKNOWN && event.getAction() == KeyEvent.ACTION_DOWN) {
             _txt5Wd220b.setBackgroundColor(SPRING_GREEN);
+            _keyPasss[4] = true;
+        }
+        if (_keyPasss[0] && _keyPasss[1] && _keyPasss[2] && _keyPasss[3] && _keyPasss[4]) {
+            _btnPass.setEnabled(true);
+            MyProgressDialogUtil.ShowCountDownTimerWarning(this, "知道了", 3 * 1000, "提示", "按键测试已通过！", false, () -> {
+                MyProgressDialogUtil.DismissAlertDialog();
+                Pass();
+            });
         }
         return false;
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,33 +96,7 @@ public class KeyDownActivity extends BaseActivity {
         _txt3Wd220b = findViewById(R.id.txt3_wd220b_keydown);
         _txt4Wd220b = findViewById(R.id.txt4_wd220b_keydown);
         _txt5Wd220b = findViewById(R.id.txt5_wd220b_keydown);
-        _txt6Wd220b = findViewById(R.id.txt6_wd220b_keydown);
-        init();
-    }
-
-    private void init() {
-        _homeKeyObserver = new HomeKeyObserver(this);
-        _homeKeyObserver.setHomeKeyListener(new HomeKeyObserver.OnHomeKeyListener() {
-            @Override
-            public void onHomeKeyPressed() {
-                Log.i(TAG, "按下Home键");
-            }
-
-            @Override
-            public void onHomeKeyLongPressed() {
-                Log.i(TAG, "长按Home键");
-            }
-        });
-        _homeKeyObserver.startListen();
-        _powerKeyObserver = new PowerKeyObserver(this);
-        _powerKeyObserver.setHomeKeyListener(new PowerKeyObserver.OnPowerKeyListener() {
-            @Override
-            public void onPowerKeyPressed() {
-                Log.i(TAG, "按下电源键");
-                _txt6Wd220b.setBackgroundColor(SPRING_GREEN);
-            }
-        });
-        _powerKeyObserver.startListen();
+        _btnPass.setEnabled(false);
     }
 
 }
