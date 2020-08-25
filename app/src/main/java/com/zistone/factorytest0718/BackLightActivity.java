0d5dd2195cc66906a66cfe372b5a26c9ea136a91
QQ652276536 <652276536@qq.com>
2020-08-25 11:23:03 +0800

    还需要加入485通信和背光亮度

---
+++ app/src/main/java/com/zistone/factorytest0718/BackLightActivity.java
@@ -0,0 +1,14 @@
+package com.zistone.factorytest0718;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BackLightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_back_light);
+    }
+}
